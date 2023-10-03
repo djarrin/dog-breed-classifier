4ff654b6ff6b42c05e84c30868a8b92cf4c7dc14
David Jarrin <dmjarrin@gmail.com>
2023-10-03 13:05:56 +0000

    Saving Progress: Debugger Setup

--- hpo.py
+++ hpo.py
@@ -7,23 +7,45 @@ import torch.optim as optim
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
+import smdebug.pytorch as smd
+from sagemaker.debugger import Rule, DebuggerHookConfig
+
 
 import argparse
 
-def test(model, test_loader):
+def test(model, test_loader, hook):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
+    hook.set_mode(smd.modes.EVAL)
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+    for data, target in test_loader:
+      data = data.view(data.shape[0], -1)
+      output = model(data)
+      pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+      correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss = len(test_loader.dataset)
+    test_accuracy = correct/test_loss
+    print(f'Test set: Accuracy: {test_accuracy} = {100*(test_accuracy)}%)')
+    
+    return test_accuracy, test_loss
+    
+
     pass
 
-def train(model, train_loader, criterion, optimizer):
+def train(model, train_loader, criterion, optimizer, hook):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
+    hook.set_mode(smd.modes.TRAIN)
+
     pass
     
 def net():
@@ -46,6 +68,12 @@ def main(args):
     '''
     model=net()
     
+    
+    # Configure debugger
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+    
+    
     '''
     TODO: Create your loss and optimizer
     '''
@@ -56,12 +84,12 @@ def main(args):
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    model=train(model, train_loader, loss_criterion, optimizer, hook)
     
     '''
     TODO: Test the model to see its accuracy
     '''
-    test(model, test_loader, criterion)
+    test(model, test_loader, criterion, hook)
     
     '''
     TODO: Save the trained model
